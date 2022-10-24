de185610bde72c64639048f7fc83c91296bae1a0
damchevski <42002981+damchevski@users.noreply.github.com>
2022-10-24 12:34:40 +0200

    without db

--- PrivateVideo/Startup.cs
+++ PrivateVideo/Startup.cs
@@ -30,12 +30,12 @@ namespace PrivateVideo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+           /* services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDefaultIdentity<PrivateUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<ApplicationDbContext>();*/
 
             //services configuration
             services.AddTransient(typeof(ISubscribeService), typeof(SubscribeService));
